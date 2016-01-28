5f6b424a7b519ab6d95fd6c136ca14f5faf4c209
Sem Karaman <skaraman@ROC02PPDUXG8WP.local>
2016-01-28 11:15:51 -0800

    update to almost everything

--- src/index.js
+++ src/index.js
@@ -11,12 +11,14 @@ var Box = require('famous/physics/bodies/Box');
 var Vec3 = require('famous/math/Vec3');
 var Quaternion = require('famous/math/Quaternion');
 var Collision = require('famous/physics/constraints/Collision');
+var Framedata = require ('./framedata.js');
 FamousEngine.init();
+var fd = Framedata.init();
 var theScene = FamousEngine.createScene();
 theScene.name = "theScene";
 document.theScene = theScene;
 var options = {};
-var world;
+var world = new physics.PhysicsEngine(options);
 var game = theScene.addChild();
 game.name = "game";
 var gameUI = game.addChild();
@@ -34,7 +36,21 @@ document.addEventListener('mousemove', function(event) {
     game.onReceive(event.type, event);
 });
 game.onReceive = function(event, payload) {
-switch(event){
+    switch(event){
+        case 'keydown':
+            switch (payload.keyCode) {
+                case 66:
+                    game.emit('sequence',payload);
+                break;
+                case 67:
+                    game.emit('reverseSequence',payload);
+                break;
+                case 65:
+                    payload.interruptable = false;
+                    game.emit('sequence_timed',payload);
+                break;
+            }
+        break;
         case 'click':
             switch (payload.node.name) {
                 case "startButtonNode":
@@ -42,6 +58,10 @@ switch(event){
                 break;
                 case "addEnemyButtonNode":
                     game.emit('addEnemyButton',payload);
+                break;
+                case "boxNode":
+                    payload.interrupt = true;
+                    game.emit('sequence',payload)
                 break;
             }
         break;
@@ -62,16 +82,17 @@ function initGame() {
     world = new physics.PhysicsEngine(options);
     world.game = game;
     game.world = world;
-
-    world.addBody(game.boxNode.box);
+    game.over = false;
 
     var gameUIChildren = gameUI.getChildren();
 
     for(var i=gameUIChildren.length;i--;){
-        if(gameUIChildren[i].name == "scoreNode"
-        || gameUIChildren[i].name == "startButtonNode"
-        || gameUIChildren[i].name == "gameOverNode"){
-            gameUI.removeChild(gameUIChildren[i]);
+        if(gameUIChildren[i] != null){
+            if(gameUIChildren[i].name == "scoreNode"
+            || gameUIChildren[i].name == "startButtonNode"
+            || gameUIChildren[i].name == "gameOverNode"){
+                gameUI.removeChild(gameUIChildren[i]);
+            }
         }
     }
     document.body.style.cursor = "none";
@@ -112,8 +133,8 @@ function initGame() {
         .setAbsoluteSize(game.sizes[0],100)
         .setAlign(0.5,0.5)
         .setPosition(-game.sizes[0]/2,-50);
-    var scoreElement = new DOMElement(scoreNode);
-    scoreElement.setProperty('font-size','60px')
+    scoreNode.DOMElement = new DOMElement(scoreNode);
+    scoreNode.DOMElement.setProperty('font-size','60px')
         .setProperty('opacity','0.5')
         .setProperty('text-align','center')
         .setContent('0');
@@ -136,6 +157,7 @@ function initGame() {
         }
     }
     scoreNode.addComponent(scoreComponent);
+    resetBody(game.boxNode.box);
 }
 function createStartButtonNode() {
     var startButtonNode = gameUI.addChild();
@@ -144,8 +166,8 @@ function createStartButtonNode() {
         .setAbsoluteSize(240,60)
         .setAlign(0.5,0.8)
         .setPosition(-120,-30);
-    var startButtonElement = new DOMElement(startButtonNode);
-    startButtonElement.setProperty('font-size','46px')
+    startButtonNode.DOMElement = new DOMElement(startButtonNode);
+    startButtonNode.DOMElement.setProperty('font-size','46px')
         .setContent('Start Game');
     startButtonNode.addUIEvent('click');
     var startComponent = {
@@ -170,23 +192,23 @@ function createBoxNode() {
     var boxNode = gameUI.addChild();
     game.boxNode = boxNode;
     boxNode.name = "boxNode";
+    boxNode.framedata = fd.framedata.test_box;
+    boxNode.idle = true;
     boxNode.setSizeMode('absolute', 'absolute')
         .setAbsoluteSize(40, 40)
         .setAlign(0.5,0.5)
         .setPosition(-20,-20,1000)
         .setOrigin(0.5, 0.5);
     boxNode.size = 80;
-    var boxNodeElement = new DOMElement(boxNode);
-    boxNodeElement.setProperty('background-color', 'blue')
-        .setProperty('z-index','1000');
+    boxNode.DOMElement = new DOMElement(boxNode);
+    boxNode.DOMElement.setProperty('background-image', 'url(./assets/grid.png)')
+        .setProperty('background-size','1100% 600%')
+        .setProperty('z-index','1000')
+        .setContent('Click Me');
+
     boxNode.addUIEvent('click');
     var boxNodePosition = boxNode.getPosition();
-    boxNode.box = new Box({
-        mass: 1,
-        size: [40,40,40],
-        position:new Vec3(boxNodePosition[0],boxNodePosition[1])
-    });
-    boxNode.box.node = boxNode;
+
     // need to add the below boxNodeComponent as a physics interation
     // on the Box, not as the below basic animation on the node
     // this is because i want the physics engine to monitor the
@@ -194,22 +216,126 @@ function createBoxNode() {
     // on the spinning corners
 
     var boxNodeComponent = boxNode.addComponent({
+        id:null,
+        node:null,
+        onMount: function(node) {
+            this.id = node.addComponent(this);
+            this.node = node;
+        },
         onUpdate: function(time) {
+            if(!boxNode.box){
+                boxNode.box = new Box({
+                    mass: 1,
+                    size: [40,40,40],
+                    position:new Vec3(boxNodePosition[0],boxNodePosition[1])
+                });
+                boxNode.box.node = boxNode;
+                world.addBody(boxNode.box);
+            }
+            if(game.started == true){
+                boxNode.setAlign(0,0)
+            }
             // set boxNode.box to spin on it's z axis
             var currentPos = boxNode.box.getPosition();
-            boxNode.box.setPosition(currentPos[0],currentPos[1],time/1000);
-            // set boxNode to update to .box position = move away from
-            // followAction()
-            boxNode.requestUpdateOnNextTick(boxNodeComponent);
+            boxNode.setPosition(currentPos.x,currentPos.y,10000);
+            boxNode.setRotation(0,0,-time/1000)
+            boxNode.requestUpdateOnNextTick(this.id);
         }
     });
-    // Let the magic begin...
+    addAnimationComponent(boxNode);
     boxNode.requestUpdate(boxNodeComponent);
-
-    boxNode.idle = true;
 }
 
-function addEnemy(speed){
+function addAnimationComponent(char){
+    var myComponent = {
+        id: null,
+        node: null,
+        done: function(node) {
+            console.log("addAnimationComponent Done ran")
+            if( typeof(node.framedata.active) == "null"){
+                node
+            }
+            node.framedata.active.frameIterator = 0;
+            node.framedata.active.msTimer = 0;
+            node.framedata.active = null;
+            node.animationTransitionable.halt();
+        },
+        onMount: function (node) {
+            this.id = node.addComponent(this);
+            this.node = node;
+        },
+        onReceive: function (event, payload) {
+            if (this.node.framedata[event]) {
+                if(!this.node.framedata.active){
+                    this.node.framedata.active = this.node.framedata[event];
+                    this.node.framedata.active.event = event;
+                    if((typeof payload.interruptable) != 'undefined')
+                        this.node.framedata.active.interruptable = payload.interruptable;
+                }
+                if((typeof this.node.framedata.active.interruptable) != 'undefined'){
+                    if(this.node.framedata.active.interruptable == false){
+                        payload.interrupt = false;
+                    }
+                }
+                if (this.node.framedata.active.frameIterator < 1 || payload.interrupt) {
+                    if(payload.interrupt){
+                        if(this.node.animationTransitionable) this.node.animationTransitionable.halt();
+                        this.node.framedata.active.frameIterator = 0;
+                    }
+                    this.node.framedata.active = this.node.framedata[event];
+                    this.node.framedata.active.event = event;
+                    if((typeof payload.interruptable) != 'undefined')
+                        this.node.framedata.active.interruptable = payload.interruptable;
+
+                    var frames = this.node.framedata.active.frames;
+                    var duration=0;
+                    for(var x=0; x < frames.length; x++){
+                        duration += frames[x].ms;
+                    }
+                    this.node.animationTransitionable = new Transitionable(0);
+                    this.node.requestUpdate(this.id);
+                    this.node.animationTransitionable.from(0).to(duration, 'linear', duration, this.done, null, this.node);
+                }
+            }
+        },
+        onUpdate: function() {
+            if(this.node.framedata.active){
+                var animation = this.node.framedata.active;
+                var frames = animation.frames;
+                var transition = this.node.animationTransitionable;
+                var grace = 2;
+                if(transition._state < 1){
+                    animation.frameIterator = 0;
+                    animation.msTimer = 0;
+                }
+                if(animation.frameIterator < frames.length){
+                    if(transition._state < (animation.msTimer + frames[animation.frameIterator].ms + grace)
+                    && transition._state >= (animation.msTimer - grace)
+                    && animation != null) {
+                        this.node.DOMElement.setProperty('background-position','-' + (frames[animation.frameIterator].x/4)
+                            + 'px ' + '-' + (frames[animation.frameIterator].y/4) + 'px')
+                        animation.msTimer += frames[animation.frameIterator].ms;
+                        animation.frameIterator++;
+                        var forceMove = transition.get();
+                        this.node.requestUpdateOnNextTick(this.id);
+                    }
+                    else if(transition.isActive()) {
+                        var forceMove = transition.get();
+                        this.node.requestUpdateOnNextTick(this.id);
+                    }
+                }else if(animation.frameIterator >= frames.length){
+                    if(transition.isActive()){
+                        transition.get();
+                        this.node.requestUpdateOnNextTick(this.id);
+                    }
+                }
+            }
+        }
+    };
+    char.addComponent(myComponent);
+}
+
+function addEnemy(speed, timing){
     var newEnemy = gameEnemies.addChild();
     newEnemy.name = "newEnemy";
     var sizes = [30,40,50,60];
@@ -236,14 +362,15 @@ function addEnemy(speed){
             newEnemy.name = "top";
         break;
     }
-    var newEnemyElement = new DOMElement(newEnemy);
+    newEnemy.DOMElement = new DOMElement(newEnemy);
     var colors = ['red','black']
-    newEnemyElement.setProperty('background',colors[Math.floor(Math.random()*colors.length)])
+    newEnemy.DOMElement.setProperty('background',colors[Math.floor(Math.random()*colors.length)])
         .setProperty('border-radius',"100%");
-    var newEnemyComponent = {
+    newEnemy.newEnemyComponent = {
         id: null,
         node: null,
         done: function(node){
+            console.log('newEnemyComponent Done ran')
             gameEnemies.removeChild(node);
             if(node._components[3] != null)
             node.removeComponent(node._components[3]);
@@ -261,12 +388,13 @@ function addEnemy(speed){
         },
         onUpdate: function(time){
             var spherePosition = this.node.sphere.getPosition();
-            if(Math.abs(spherePosition.x) > gameSize[0] || Math.abs(spherePosition.y) > gameSize[1]){
-                this.done(this.node);
-            }else{
+            /*if(Math.abs(spherePosition.x) > gameSize[0] || Math.abs(spherePosition.y) > gameSize[1]){
+                if(this.node._id != null)
+                    this.done(this.node);
+            }else{*/
                 this.node.setPosition(spherePosition.x,spherePosition.y);
                 this.node.requestUpdateOnNextTick(this.id);
-            }
+            //}
         }
     };
     var newEnemyPosition = newEnemy.getPosition();
@@ -278,7 +406,7 @@ function addEnemy(speed){
     newEnemy.sphere.node = newEnemy;
     world.addBody(newEnemy.sphere);
     world.addConstraint(new Collision([game.boxNode.box,newEnemy.sphere],{restitution:0}));
-    newEnemy.addComponent(newEnemyComponent);
+    newEnemy.addComponent(newEnemy.newEnemyComponent);
     switch (newEnemy.name) {
         case "left":
             newEnemy.sphere.setVelocity(speed,0);
@@ -295,19 +423,52 @@ function addEnemy(speed){
     }
     var payload={};
     game.emit('animateEnemies',payload);
+    FamousEngine.getClock().setTimeout(function(){
+      addEnemyUtil();
+      collisionDetection();
+    },timing);
 }
 function addEnemyUtil(){
     if(gameEnemies._components[3]){
         gameEnemies.removeComponent(gameEnemies._components[3]);
     }
-    var timings_teir1 = [400,500];
-    var timing = timings_teir1[Math.floor(Math.random()*timings_teir1.length)];
-
-    game.addEnemyInterval = FamousEngine.getClock().setInterval(function(){
-        addEnemy(300)
-    },timing);
+    if (game.over == false) {
+        var timings_teirs = [
+          [600,800],
+          [500,700],
+          [400,600],
+          [300,500],
+          [200,300],
+          [100,200]
+        ];
+        // speed should be determined by a factor of the score and is a measure
+        // of how fast the enemy will be
+        var speed = 300; // 300 + (random * score)
+        // timing should be determined by a factor of the score and is a measure
+        // of how quickly new enemy will be created
+        var i=0;
+        if (game.score < 1000 ){
+            i=0
+        }else if (game.score < 2000 ) {
+            i=1;
+        }else if (game.score < 3000) {
+            i=2;
+        }else if (game.score < 4000) {
+            i=3;
+        }else if (game.score < 5000) {
+            i=4;
+        }else if (game.score > 5001) {
+            i=5;
+        }
+        var timing_teir = timings_teirs[i];
+        var timing = Math.floor(Math.random()*(timing_teir[1] - timing_teir[0])) + timing_teir[0];
+        addEnemy(speed,timing)
+    }
 }
 function followAction(){
+    if(!game.boxNode.box){
+        createBoxNode();
+    }
     var newPosX, newPosY
     if(event.type == "touchmove"){
         newPosX = event.touches[0].clientX - (game.boxNode.size/2);
@@ -318,8 +479,6 @@ function followAction(){
     }
     var currentPos = game.boxNode.getPosition();
     if(game.boxNode.idle == true){
-        game.boxNode.setAlign(0,0,0);
-        game.boxNode.setPosition(newPosX,newPosY);
         game.boxNode.box.setPosition(newPosX,newPosY,0);
         game.boxNode.idle = false;
     } else {
@@ -327,24 +486,25 @@ function followAction(){
         xDiff = Math.abs(currentPos[0] - newPosX);
         yDiff = Math.abs(currentPos[1] - newPosY);
         if(xDiff > 200 || yDiff > 200 ){
-            game.boxNode.position = new Position(game.boxNode);
-            game.boxNode.position.set(newPosX,newPosY,10000,{duration:1000});
-            game.boxNode.position.onUpdate;
+            game.boxNode.box.position = new Position(game.boxNode);
+            game.boxNode.box.position.set(newPosX,newPosY,10000,{duration:1000});
+            game.boxNode.box.position.onUpdate;
         }else if((game.boxNode.position && !game.boxNode.position.isActive())||!game.boxNode.position){
-            game.boxNode.setPosition(newPosX,newPosY);
             game.boxNode.box.setPosition(newPosX,newPosY,0);
         }
     }
 }
 function updateScore(score,node){
-    var numScore = parseInt(score._components[3]._content);
-    var newScore = numScore + 10;
+    var numScore = parseInt(score.DOMElement._content);
+    var newScore = numScore + node.getSize()[0];
     game.score = newScore;
-    score._components[3].setContent(newScore);
-    node._components[4].done(node);
+    score.DOMElement.setContent(newScore);
+    if(node._id != null){
+        node.newEnemyComponent.done(node);
+    }
 }
 function gameOver(){
-    FamousEngine.getClock().clearTimer(game.addEnemyInterval);
+    game.over = true;
     var gC = gameUI.getChildren()
     var score = gC[1];
     var gameOverNode = gameUI.addChild();
@@ -353,27 +513,30 @@ function gameOver(){
         .setAbsoluteSize(400,100)
         .setAlign(0.5,0.5)
         .setPosition(-200,-50);
-    var gameOverElement = new DOMElement(gameOverNode);
-    gameOverElement.setProperty('font-size','64px')
+    gameOverNode.DOMElement = new DOMElement(gameOverNode);
+    gameOverNode.DOMElement.setProperty('font-size','64px')
         .setProperty('text-align','center')
         .setContent('Game Over');
 
     createStartButtonNode();
 
     score.setAlign(0.5,0.3,0);
-    score._components[3].setProperty('opacity','1.0');
+    score.DOMElement.setProperty('opacity','1.0');
     game.started = false;
-    boxNode.setAlign(0.5,0.7)
+    game.boxNode.setAlign(0.5,0.7)
         .setPosition(-20,-20,1000);
-    boxNode.idle = true;
+    game.boxNode.idle = true;
     document.body.style.cursor = "auto";
 
 
     var allEnemies = gameEnemies.getChildren();
     while(allEnemies.length){
-        allEnemies[0].removeComponent(allEnemies[0]._components[3]);
+        if(allEnemies[0]==null){
+            allEnemies.splice(0,1)
+            continue;
+        }
+        allEnemies[0].removeComponent(allEnemies[0].DOMElement);
         Dismount(allEnemies[0]);
-        //gameEnemies.removeChild(allEnemies[0]);
     }
     gameEnemies.requestUpdate(gameEnemies._components[3].id);
 }
@@ -400,8 +563,8 @@ function collisionDetection(){
                 && !world.constraints[i].detected){
                     world.constraints[i].detected = true;
                     resetBody(game.boxNode.box);
-                    if(world.constraints[i].targets[1].node._components[3]){
-                        var enemyType = world.constraints[i].targets[1].node._components[3]._styles.background;
+                    if(world.constraints[i].targets[1].node.DOMElement){
+                        var enemyType = world.constraints[i].targets[1].node.DOMElement._styles.background;
                         if(enemyType == 'black'){
                             console.log('hit black!');
                             var payload = {};
@@ -421,7 +584,6 @@ function collisionDetection(){
                     world.removeConstraint(world.constraints[i]);
                 }
             }
-            console.log(time);
             world.update(time);
             this.node.requestUpdateOnNextTick(this.id);
         }
@@ -429,21 +591,17 @@ function collisionDetection(){
     gameEnemies.addComponent(collisionComponent);
     gameEnemies.requestUpdate(gameEnemies._components[3].id);
 }
-
 var Dismount = function(node) {
     var aNodes = [];
     if ( !(node instanceof Node) )
         throw "node not a Famous#Node";
-
     var f = function(current) {
         aNodes.push(current);
         var aChildren = current.getChildren();
         for ( var i in aChildren )
             f(aChildren[i]);
     };
-
     f(node);
-
     while ( aNodes.length ) {
         var x = aNodes.pop();
         if ( x.isMounted())
